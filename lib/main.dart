7638994fbf9e28bacc8054afc9815cda02b72d04
iswift-ru <cto@iSwift.ru>
2020-04-16 17:36:37 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,270 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hex_color/flutter_hex_color.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registracija',
+      theme: ThemeData(
+          primaryColor: HexColor('00539C'),
+          textTheme: TextTheme(
+              title: TextStyle(color: HexColor('FFFFFF'), fontSize: 16.0))),
+      home: RegistrationPage(),
+    );
+  }
+}
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget daysOfTheWeek() {
+      return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'PR',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'AN',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'TR',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'KT',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'PN',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'ST',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 36.0,
+              width: 36,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Text(
+                'SK',
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white, letterSpacing: 0.4),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 166, left: 16),
+            child: Text(
+              'Pasirinkite darbo laiko nustatymus. \nNustatymai gali būti pakeičiami vėliau.',
+              style: TextStyle(
+                color: HexColor('FFFFFF'),
+                fontSize: 16.0,
+                letterSpacing: 0.15,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 16),
+            child: Text(
+              'Darbo dienos',
+              style: TextStyle(
+                color: HexColor('FFFFFF'),
+                fontSize: 14.0,
+                letterSpacing: 0.15,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          daysOfTheWeek(),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: new Divider(
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Pradžia',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                Text('8:00',
+                    style: TextStyle(fontSize: 14, color: Colors.white))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text('Pirmas vizitas dienoje',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color.fromRGBO(255, 255, 255, 0.7),
+                  letterSpacing: 0.4,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Pabaiga',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                Text('17:00',
+                    style: TextStyle(fontSize: 14, color: Colors.white))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Text('Paskutinio vizito pabaiga',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color.fromRGBO(255, 255, 255, 0.7),
+                  letterSpacing: 0.4,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 80),
+            child: new Divider(
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Atostogos',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                Text('PRIDETI',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: HexColor('FBC02D'),
+                        letterSpacing: 0.75))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Text('Neprivaloma pridėti',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color.fromRGBO(255, 255, 255, 0.7),
+                  letterSpacing: 0.4,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: new Divider(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
