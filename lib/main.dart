e8bf903346ac478df5b9bb8862fdabf2cbf6b2e3
iswift-ru <cto@iSwift.ru>
2020-04-19 14:04:50 +0300

    0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hex_color/flutter_hex_color.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,33 +8,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Registracija',
       theme: ThemeData(
-          primaryColor: HexColor('00539C'),
+          primaryColor: Color(0xFF00539C),
           textTheme: TextTheme(
-              title: TextStyle(color: HexColor('FFFFFF'), fontSize: 16.0))),
-      home: MoyVariant(),
+              title: TextStyle(color: Color(0xFFFFFFFF), fontSize: 16.0))),
+      home: DayOfWeek(),
     );
   }
 }
 
-///////////////////
-
-class MoyVariant extends StatefulWidget {
-  MoyVariant({Key key}) : super(key: key);
-
+class DayOfWeek extends StatefulWidget {
   @override
-  _MoyVariantState createState() => _MoyVariantState();
+  _DayOfWeekState createState() => _DayOfWeekState();
 }
 
-class _MoyVariantState extends State<MoyVariant> {
-  List<String> reportList = ['PR', 'AN', 'TR', 'KT', 'PN', 'ST', 'SK'];
-
+class _DayOfWeekState extends State<DayOfWeek> {
   List<String> selectedReportList = List();
+  List<String> reportList = ['PR', 'AN', 'TR', 'KT', 'PN', 'ST', 'SK'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: HexColor('00539C'),
+        color: Color(0xFF00539C),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
@@ -45,18 +38,18 @@ class _MoyVariantState extends State<MoyVariant> {
               child: Text(
                 'Pasirinkite darbo laiko nustatymus. \nNustatymai gali būti pakeičiami vėliau.',
                 style: TextStyle(
-                  color: HexColor('FFFFFF'),
+                  color: Color(0xFFFFFFFF),
                   fontSize: 16.0,
                   letterSpacing: 0.15,
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 16, left: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text(
                 'Darbo dienos',
                 style: TextStyle(
-                  color: HexColor('FFFFFF'),
+                  color: Color(0xFFFFFFFF),
                   fontSize: 14.0,
                   letterSpacing: 0.15,
                 ),
@@ -74,13 +67,13 @@ class _MoyVariantState extends State<MoyVariant> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16),
-              child: new Divider(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Divider(
                 color: Colors.white,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -94,7 +87,7 @@ class _MoyVariantState extends State<MoyVariant> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text('Pirmas vizitas dienoje',
                   style: TextStyle(
                     fontSize: 12,
@@ -117,7 +110,7 @@ class _MoyVariantState extends State<MoyVariant> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text('Paskutinio vizito pabaiga',
                   style: TextStyle(
                     fontSize: 12,
@@ -127,7 +120,7 @@ class _MoyVariantState extends State<MoyVariant> {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 16, right: 16, top: 0),
-              child: new Divider(
+              child: Divider(
                 color: Colors.white,
               ),
             ),
@@ -143,13 +136,13 @@ class _MoyVariantState extends State<MoyVariant> {
                   Text('PRIDETI',
                       style: TextStyle(
                           fontSize: 14,
-                          color: HexColor('FBC02D'),
+                          color: Color(0xFFFBC02D),
                           letterSpacing: 0.75))
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text('Neprivaloma pridėti',
                   style: TextStyle(
                     fontSize: 12,
@@ -159,15 +152,8 @@ class _MoyVariantState extends State<MoyVariant> {
             ),
             Padding(
               padding: const EdgeInsets.all(16),
-              child: new Divider(
+              child: Divider(
                 color: Colors.white,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                "Select : " + selectedReportList.join(" , "),
-                style: TextStyle(color: Colors.white),
               ),
             ),
           ],
@@ -194,34 +180,22 @@ class _MultiSelectChipState extends State<MultiSelectChip> {
     List<Widget> choices = List();
 
     widget.reportList.forEach((item) {
-      choices.add(Container(
-        alignment: Alignment.center,
-        height: 46.0,
-        width: 46,
-        /*decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              25.0,
-            ),
-            border: Border.all(width: 1, color: Colors.white))*/
-        //padding: const EdgeInsets.all(10.0),
-        child: ChoiceChip(
-          //backgroundColor: HexColor('00539C'),
-          label: Text(
-            item,
-          ),
-          labelStyle: TextStyle(color: Colors.blue, fontSize: 12),
-          selected: selectedChoices.contains(item),
-          selectedColor: Colors.green,
-
-          onSelected: (selected) {
-            setState(() {
-              selectedChoices.contains(item)
-                  ? selectedChoices.remove(item)
-                  : selectedChoices.add(item);
-              widget.onSelectionChanged(selectedChoices);
-            });
-          },
-        ),
+      choices.add(ChoiceChip(
+        label: Text(item),
+        labelStyle: TextStyle(color: Colors.white, fontSize: 12),
+        selected: selectedChoices.contains(item),
+        shape: selectedChoices.contains(item)
+            ? CircleBorder(side: BorderSide(color: Colors.white, width: 1))
+            : CircleBorder(side: BorderSide.none),
+        backgroundColor: Color(0xFF00539C),
+        selectedColor: Color(0xFF00539C),
+        onSelected: (selected) {
+          setState(() {
+            selectedChoices.contains(item)
+                ? selectedChoices.remove(item)
+                : selectedChoices.add(item);
+          });
+        },
       ));
     });
     return choices;
@@ -235,40 +209,3 @@ class _MultiSelectChipState extends State<MultiSelectChip> {
     );
   }
 }
-
-/*class CustomChipLabel extends StatefulWidget {
-  @override
-  _CustomChipLabelState createState() => _CustomChipLabelState();
-}
-
-class _CustomChipLabelState extends State<CustomChipLabel> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Padding(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            height: 36.0,
-            width: 36,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  25.0,
-                ),
-                border: Border.all(width: 1, color: Colors.white)),
-            child: Text(
-              "item",
-              style: TextStyle(
-                  fontSize: 12, color: Colors.white, letterSpacing: 0.4),
-            ),
-          )
-        ],
-      ),
-    ));
-  }
-}
-*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////
